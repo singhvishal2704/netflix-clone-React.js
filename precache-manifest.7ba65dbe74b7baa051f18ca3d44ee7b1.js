75ea411a57dc9f401f771b9d31e053219733a7ab
singhvishal2704 <singhvishal2704@gmail.com>
2020-10-27 21:58:29 +0530

    Updates

---
+++ precache-manifest.7ba65dbe74b7baa051f18ca3d44ee7b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22d5a00492f893e65a735a386e9da92f",
+    "url": "/netflix-clone-React.js/index.html"
+  },
+  {
+    "revision": "b8cc7c9729529e4c0992",
+    "url": "/netflix-clone-React.js/static/css/main.9f204a85.chunk.css"
+  },
+  {
+    "revision": "2105e77cae4894261294",
+    "url": "/netflix-clone-React.js/static/js/2.56378d79.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone-React.js/static/js/2.56378d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8cc7c9729529e4c0992",
+    "url": "/netflix-clone-React.js/static/js/main.ecc3fc8d.chunk.js"
+  },
+  {
+    "revision": "d9b6e536823dd22fe1b3",
+    "url": "/netflix-clone-React.js/static/js/runtime-main.817cd51e.js"
+  }
+]);
